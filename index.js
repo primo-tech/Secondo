77233c8fac10558460572d03e04217c81e580d02
Alexander Dada <alexdada555@gmail.com>
2019-09-07 23:35:18 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,55 @@
+const Discord = require('discord.js');
+const { prefix, token } = require('./config.json');
+const client = new Discord.Client();
+
+client.once('ready', () =>
+{
+	console.log('Ready!')
+});
+
+client.on('message', message =>
+{
+	if(message.member.hasPermission(['KICK_MEMBERS','BAN_MEMBERS']))
+	{
+		if(message.content.startsWith(`${prefix}hello`))
+		{
+			message.channel.send("Greetings Ningen!");
+		}
+
+		else if(message.content.startsWith(`${prefix}kick`))
+		{
+			let user = message.mentions.members.first();
+			const general = message.guild.channels.find(ch => ch.name === 'general');
+
+			user.kick().then((user) =>
+			{
+				general.send(`Sayounara, ${user.displayName}`);
+			});
+		}
+	
+		else if(message.content.startsWith(`${prefix}ban`))
+		{
+			let user = message.mentions.members.first();
+
+			const general = message.guild.channels.find(ch => ch.name === 'general');
+			
+			user.ban().then((user) =>
+			{
+				general.send(`Omae Wa Mou, SHINDEIRU!!!!, ${user.displayName} !`);
+			});
+		}
+		
+		else if(message.content.startsWith(`${prefix}test`))
+                {
+                        let user = message.mentions.members.first();
+
+                        const general = message.guild.channels.find(ch => ch.name === 'general');
+                        
+                        general.send(`Omae Wa Mou, SHINDEIRU!!!!, ${user.displayName} !`);
+                }
+
+	}
+});
+
+client.login(token);
+
